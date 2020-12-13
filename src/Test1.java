d16a6f5e7abd7370be0b6cb10b93961dc3e6a18c
swapnalij1 <swapnalij4050@gmail.com>
2020-12-13 14:57:09 +0530

    User1

---
+++ src/Test1.java
@@ -0,0 +1,13 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("a");
+		System.out.println("b ");
+		System.out.println("c");
+		System.out.println("d");
+
+	}
+
+}
